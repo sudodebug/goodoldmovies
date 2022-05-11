e812adb16d81fdf2cf1dcad1edcfed44d9a918f8
Sudo debug <sudodebug@gmail.com>
2022-05-11 17:33:02 +0500

    create exception

--- conf/urls.py
+++ conf/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from movies.views import movie_view
+from movies.views import movie_view, custom_handler404, custom_handler400, custom_handler403, custom_handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', movie_view, name='main'),
+    path('movie/<int:movie_id>', movie_view),
 ]
+
+handler400 = custom_handler400
+handler403 = custom_handler403
+handler404 = custom_handler404
+handler500 = custom_handler500
+
--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.views import View
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseServerError
 
 movies = [
     {'title': 'Catchfire', 'year': 1990, },
     {'title': 'Mighty Ducks the Movie: The First Face-Off', 'year': 1997, },
     {'title': 'Le zombie de Cap-Rouge', 'year': 1997, },
-    ]
+]
+
+movie_data = {
+    1: 'The dark knight',
+    2: '12 Angry men',
+    3: 'Il brutto, il cattivo',
+}
 
 
-def movie_view(request):
-    context = {'movies': movies}
-    return render(request, 'movies/index.html', context=context)
+def movie_view(request, movie_id):
+    movie_title = movie_data.get(movie_id)
+    return HttpResponse(f'This is {movie_title}')
 
+
+def custom_handler400(request, exception):
+    return HttpResponseBadRequest('Неверный запрос!')
+
+
+def custom_handler403(request, exception):
+    return HttpResponseForbidden('Доступ запрещен!')
+
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('Ресурс не найден!')
+
+
+def custom_handler500(request):
+    return HttpResponseServerError('Ошибка сервера!')
